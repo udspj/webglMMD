a48db75fc2efdfc9b1e01bcdc31883cf61d61813
sunpeijia <1311107143@qq.com>
2018-10-24 16:41:17 +0800

    gl底跑起来

--- mmdtest/src/index.js
+++ mmdtest/src/index.js
@@ -1,1 +1,29 @@
-document.write("hello world222");
+
+var gl; // WebGL的全局变量
+
+window.onload = function(){
+	var canvas = document.getElementById("glcanvas");
+	gl = initWebGL(canvas);   
+	if (!gl) {
+		alert('gl error');
+		return;
+	}
+
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);  
+	gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+	gl.enable(gl.DEPTH_TEST);
+}
+
+function initWebGL(canvas) {
+	window.gl = null;
+	try {
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+	}
+	catch(e) {}
+
+	if (!gl) {
+		alert("WebGL初始化失败，可能是因为您的浏览器不支持。");
+		gl = null;
+	}
+	return gl;
+}
