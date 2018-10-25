a56334bc658810195b301cd3bad191296fe1962d
sunpeijia <1311107143@qq.com>
2018-10-25 20:57:30 +0800

    no message

--- mmdtest/src/utils.js
+++ mmdtest/src/utils.js
@@ -2,3 +2,4 @@ import exec from 'script-loader!./lib/cuon-utils.js';
 import exec1 from 'script-loader!./lib/cuon-matrix.js';
 import exec2 from 'script-loader!./shadersource.js';
 import exec3 from 'script-loader!./vertices.js';
+
