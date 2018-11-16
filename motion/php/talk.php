a16cbad31ab9b64f6a4999d4bea9ea657f71d5db
sunpeijia <1311107143@qq.com>
2018-11-16 16:34:56 +0800

    no message

--- motion/php/talk.php
+++ motion/php/talk.php
@@ -2,28 +2,23 @@
 
 header("Access-Control-Allow-Origin: *");
 
+session_start(); 
+
+$inputText = trim($_POST['inputText']); // "天气";//
+
 $sth = '{
-	"reqType":0,
+    "reqType":0,
     "perception": {
         "inputText": {
-            "text": "天气"
-        },
-        "inputImage": {
-            "url": "imageUrl"
-        },
-        "selfInfo": {
-            "location": {
-                "city": "上海",
-                "province": "上海",
-                "street": "信息路"
-            }
+            "text": "'.$inputText.'"
         }
     },
     "userInfo": {
         "apiKey": "cf5fb4b68b69473d91d447667194f578",
         "userId": "348368"
     }
-}'
+}';
+// echo $sth;
 
 $url = "http://openapi.tuling123.com/openapi/api/v2";    
 $content = $sth;//json_encode("your data to be sent");
@@ -40,15 +35,15 @@ $json_response = curl_exec($curl);
 
 $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-if ( $status != 201 ) {
-    die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
-}
+// if ( $status != 201 ) {
+//     die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+// }
 
 
 curl_close($curl);
 
-$response = json_decode($json_response, true);
+// $response = json_decode($json_response, true);
 
-print $response;
+echo $json_response;
 
 ?>
